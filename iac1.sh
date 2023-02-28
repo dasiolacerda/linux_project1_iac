e8e73c10f116581281076029cae64be6e8b80f30
Dasio Silva Lacerda <dlacerda@gmail.com>
2023-02-28 15:05:42 -0300

    aquivo de IAC v.1 Dio

---
+++ iac1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Removmendo diretórios e conteudo... "
+rm -rf /public /adm /ven /sec
+
+echo "Criando diretórios..."
+mkdir /public /adm /ven /sec
+
+echo "Criando grupos..."
+groupadd GRP_ADM 
+groupadd GRP_VEN 
+groupadd GRP_SEC 
+
+echo "Espcifdicando permissões de diretórios..."
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm /ven /sec
+chmod 777 /public
+
+echo "crianco Usuários..."
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
